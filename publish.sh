42b8fbc514b0c3ae7fba079de9b31756cd6d03e6
hkim <mfiacp@hanmail.net>
2024-03-26 18:30:15 +0900

    도커 이미지 이름 변경

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker cp silgerae:/opt/myproject/db/silgerae.db /volume1/homes/brokim/silgerae/db
 docker rm -f silgerae
-docker rmi silgerae
+docker rmi silgerae2
 docker build -t silgerae --name Dockerfile_silgerae .
 docker run -d -p 8000:8000 --name silgerae2 silgerae
