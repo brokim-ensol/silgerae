6fbb750f86b9a746b96e18d433ea9b54358dfd49
hkim <mfiacp@hanmail.net>
2024-03-26 19:24:07 +0900

    Dockerfile_silgerae를 사용하여 Docker 이미지 빌드하도록 수정

--- publish.sh
+++ publish.sh
@@ -13,7 +13,7 @@ if [ "$(docker images -q silgerae)" ]; then
 fi
 git pull
 echo "=> Building the image"
-docker build -t silgerae --name Dockerfile_silgerae .
+docker build -t silgerae --file Dockerfile_silgerae .
 echo "=> Running the container"
 docker run -d -p 8000:8000 --name silgerae silgerae
 echo "=> Done"
